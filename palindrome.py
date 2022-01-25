057724d1744ca3e4295302cebd6b11af44656b41
harshchudasama365 <58543580+harshchudasama365@users.noreply.github.com>
2022-01-25 13:20:19 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,1 @@
-# function which return reverse of a string
-
-def isPalindrome(s):
-	return s == s[::-1]
-
-
-# Driver code
-s = "malayalam"
-ans = isPalindrome(s)
-
-if ans:
-	print("Yes")
-else:
-	print("No")
+print('Hello World !!!')
